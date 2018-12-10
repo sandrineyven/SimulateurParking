75494459dc29b4179d76d10c30846cf08f4c865e
fhy01 <fadoua.hamouachy@live.fr>
2018-12-10 13:54:07 -0500

    Archi

---
+++ src/Capteur.java
@@ -0,0 +1,18 @@
+
+public class Capteur {
+	int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Capteur(int id) {
+		super();
+		this.id = id;
+	}
+
+}
---
+++ src/Filtre.java
@@ -0,0 +1,22 @@
+
+public class Filtre {
+	int id;
+		
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void filtreF1(){
+		
+	}
+	
+	public void filtreF2(){
+		
+	}
+	
+}
