8be67feb4b8445fb1636109e72495fbd793fa43c
Sandrine <sandrine.yven@gmail.com>
2018-12-13 17:37:58 -0500

    thread start

--- src/Capteur.java
+++ src/Capteur.java
@@ -18,22 +18,15 @@ public class Capteur {
 		this.id = id;
 	}
 
-	public Capteur(int id) {
+	@SuppressWarnings("null")
+	public Capteur(int id, int frequence, String nomFichierCapteur) {
 		this.id = id;
-		try {
-			recupFichierCapteur(id);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		try {
-			this.valeur = actualiser();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		this.frequence = frequence;
+		this.nomFichierCapteur = nomFichierCapteur;
 	}
 
 	//Lecture de la valeur du capteur
-	public boolean actualiser() throws IOException {
+	public void actualiser() throws IOException {
 		BufferedReader lecteurAvecBuffer = null;
 		String ligne;
 
@@ -44,58 +37,16 @@ public class Capteur {
 		}
 		while ((ligne = lecteurAvecBuffer.readLine()) != null) {
 			if (ligne.charAt(ligne.length() - 1) == '1') {
-				return true;
-			}
-		}
-		lecteurAvecBuffer.close();
-		return false;
-	}
-
-	//Recuperation du bon fichier selon le capteur choisi
-	public void recupFichierCapteur(int id) throws IOException {
-		this.nomFichierCapteur = null;
-		String nomFichierParam = null;
-		switch (id) {
-		case 0:
-			nomFichierParam = "res/sd0.txt";
-			break;
-		case 1:
-			nomFichierParam = "res/sd1.txt";
-			break;
-		case 2:
-			nomFichierParam = "res/sd2.txt";
-			break;
-		case 3:
-			nomFichierParam = "res/sd3.txt";
-			break;
-		}
-		if (nomFichierParam != null) {
-			recupParam(nomFichierParam);
-		} else {
-			System.out.println("Fichier introuvable");
-		}
-	}
-
-	//Recuperation de la frequence a laquelle on va lire les valeurs du capteur
-	public void recupParam(String nomFichier) throws IOException {
-		BufferedReader lecteurAvecBuffer = null;
-		String ligne;
-		int nbLigne = 0;
-		try {
-			lecteurAvecBuffer = new BufferedReader(new FileReader(nomFichier));
-		} catch (FileNotFoundException exc) {
-			System.out.println("Erreur d'ouverture");
-		}
-		while ((ligne = lecteurAvecBuffer.readLine()) != null) {
-			nbLigne++;
-			if (nbLigne == 2) {
-				this.frequence = Integer.parseInt(ligne.substring(6, ligne.length()));
-			}else if(nbLigne == 3) {
-				this.nomFichierCapteur = "res" + ligne.substring(7, ligne.length()) +".txt";
+				this.valeur = true;
+			}else {
+				this.valeur = false;
 			}
 		}
 		lecteurAvecBuffer.close();
 	}
+
+
+	
 	
 	//Getteurs et setteurs
 
--- src/Gestion.java
+++ src/Gestion.java
@@ -1,24 +1,57 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Gestion{
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		
 		Filtre filtre = new Filtre();
-//		Capteur capteur0 = new Capteur(lecture0[0], lecture0[1], lecture0[2]);
-//		Capteur capteur1 = new Capteur(lecture1[0], lecture1[1], lecture1[2]);
-//		Capteur capteur2 = new Capteur(lecture2[0], lecture2[1], lecture2[2]);
-//		Capteur capteur3 = new Capteur(lecture3[0], lecture3[1], lecture3[2]);
-	
-		
-		Capteur capteur0 = new Capteur(0);
-		Capteur capteur1 = new Capteur(1);
-		Capteur capteur2 = new Capteur(2);
-		Capteur capteur3 = new Capteur(3);
+		String[] lecture0 = recupParam("res/sd0.txt");
+		String[] lecture1 = recupParam("res/sd1.txt");
+		String[] lecture2 = recupParam("res/sd2.txt");
+		String[] lecture3 = recupParam("res/sd3.txt");
+		Capteur capteur0 = new Capteur(Integer.parseInt(lecture0[0]),Integer.parseInt(lecture0[1]), lecture0[2]);
+		Capteur capteur1 = new Capteur(Integer.parseInt(lecture1[0]),Integer.parseInt(lecture1[1]), lecture1[2]);
+		Capteur capteur2 = new Capteur(Integer.parseInt(lecture2[0]),Integer.parseInt(lecture2[1]), lecture2[2]);
+		Capteur capteur3 = new Capteur(Integer.parseInt(lecture3[0]),Integer.parseInt(lecture3[1]), lecture3[2]);
+
 		
 		Serveur service0 = new Serveur(capteur0, filtre, 0, false);
 		Serveur service1 = new Serveur(capteur1, filtre, 1, false);
 		Serveur service2 = new Serveur(capteur2, filtre, 2, false);
 		Serveur service3 = new Serveur(capteur3, filtre, 3, false);
+		
+		service0.start();
+		service1.start();
+		service2.start();
+		service3.start();
 	
 	}
+	
+	//Recuperation de la frequence a laquelle on va lire les valeurs du capteur
+		public static String[] recupParam(String nomFichier) throws IOException {
+			String[] lecture= new String[3];
+			BufferedReader lecteurAvecBuffer = null;
+			String ligne;
+			int nbLigne = 0;
+			try {
+				lecteurAvecBuffer = new BufferedReader(new FileReader(nomFichier));
+			} catch (FileNotFoundException exc) {
+				System.out.println("Erreur d'ouverture");
+			}
+			while ((ligne = lecteurAvecBuffer.readLine()) != null) {
+				nbLigne++;
+				if (nbLigne == 1) {
+					lecture[0] = ligne.substring(6, ligne.length());
+				}else if(nbLigne == 2) {
+					lecture[1] = ligne.substring(6, ligne.length());
+				}else if(nbLigne == 3) {
+					lecture[2] = "res" + ligne.substring(7, ligne.length()) +".txt";
+				}
+			}
+			lecteurAvecBuffer.close();
+			return lecture;
+		}
 }
